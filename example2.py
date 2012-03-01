b5719a8c5223604adc8bcb9f68c6bac71fd53c65
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-03-01 16:09:09 -0300

    [python] Add module type listing example
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

---
+++ example2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import sys, os, xmp
+
+if len(sys.argv) < 2:
+	print "Usage: %s <module>" % (os.path.basename(sys.argv[0]))
+	sys.exit(1)
+
+info = xmp.struct_xmp_test_info()
+
+x = xmp.Xmp()
+
+for name in sys.argv[1:]:
+	if x.testModule(name, info):
+		print "%-25.25s %-25.25s %s" % (name, info.type, info.name)
+
+
