2b3cf2dc8cd6749affe440cc853ce5d993387385
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-03-06 10:15:28 -0300

    [python] Make ctypesgen generate xmp.py directly
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 
-libxmp.py:
-	ctypesgen/ctypesgen.py -lxmp ../include/xmp.h -o libxmp.py
+xmp.py: ../include/xmp.h interface.py Makefile
+	ctypesgen/ctypesgen.py -lxmp ../include/xmp.h -o $@ --insert-file=interface.py
 	sed -i 's/^xmp_context = String/xmp_context = c_long/' $@
+	sed -i '/^# \/.*\/include\/xmp.h: \d*/d;s/\s*# \/.*\/include\/xmp.h: \d*.*//' $@
+
---
+++ interface.py
@@ -0,0 +1,53 @@
+
+class Xmp:
+	def __init__(self):
+		self._ctx = xmp_create_context()
+
+	def __del__(self):
+		if self._ctx != None:
+			self.free()
+
+	def free(self):
+		xmp_free_context(self._ctx)
+		self._ctx = None
+
+	def testModule(self, path, info):
+		code = xmp_test_module(path, pointer(info))
+		return (code == 0)
+
+	def loadModule(self, path):
+		code = xmp_load_module(self._ctx, path)
+		if (code < 0):
+			code = -code
+			raise IOError(code, os.strerror(code))
+	
+	def releaseModule(self):
+		xmp_release_module(self._ctx)
+
+	def playerStart(self, start, freq, mode):
+		return xmp_player_start(self._ctx, start, freq, mode)
+
+	def getInfo(self, info):
+		return xmp_player_get_info(self._ctx, pointer(info))
+
+	def getBuffer(self, info):
+		buf = ctypes.cast(info.buffer, POINTER(c_int8))
+		return ctypes.string_at(buf, info.buffer_size);
+
+	def playerFrame(self):
+		return xmp_player_frame(self._ctx) == 0
+
+	def playerEnd(self):
+		xmp_player_end(self._ctx)
+
+	def getSample(self, mod, num):
+		sample = mod.xxs[num]
+		buf = ctypes.cast(sample.data, POINTER(c_int8))
+
+		if sample.flg & XMP_SAMPLE_16BIT:
+			width = 2
+		else:
+			width = 1
+
+		return ctypes.string_at(buf, sample.len * width)
+
