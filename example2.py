e0c357838646bacb7b59a98abb434cc451036ca4
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-04-11 10:51:31 -0300

    [python] Example 2 code style changes
    Change coding style to follow guidelines.
    
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- example2.py
+++ example2.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python
-#
-# Example 2: identify module files
+"""
+Example 2: identify module files
+"""
 
-import sys, os, xmp
+import sys
+import os
+import xmp
 
 if len(sys.argv) < 2:
-	print "Usage: %s <module>" % (os.path.basename(sys.argv[0]))
-	sys.exit(1)
+    print "Usage: %s <module>" % (os.path.basename(sys.argv[0]))
+    sys.exit(1)
 
 info = xmp.struct_xmp_test_info()
 
 x = xmp.Xmp()
 
 for name in sys.argv[1:]:
-	if x.testModule(name, info):
-		print "%-25.25s %-25.25s %s" % (name, info.type, info.name)
+    if x.testModule(name, info):
+        print "%-25.25s %-25.25s %s" % (name, info.type, info.name)
 
 
