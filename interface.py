267c98c2420971be79fe9afdbba3867158ba4269
Claudio Matsuoka <cmatsuoka@gmail.com>
2013-06-16 17:09:10 -0300

    Add libxmp 4.1 API interface methods
    Implement the rest of the libxmp 4.1 calls. Ideally we should add a set
    of tests to see if they're performing correctly.
    
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- interface.py
+++ interface.py
@@ -7,6 +7,9 @@ class Xmp:
 	Tracker III S3M, Fast Tracker II XM and Impulse Tracker IT modules.
 
 	"""
+
+	# Regular C API calls for libxmp 4.1
+
 	def __init__(self):
 		self._ctx = xmp_create_context()
 
@@ -28,27 +31,75 @@ class Xmp:
 			code = -code
 			raise IOError(code, os.strerror(code))
 	
+	def scan_module(self):
+		xmp_scan_module(self._ctx)
+
 	def release_module(self):
 		xmp_release_module(self._ctx)
 
 	def start_player(self, freq, mode):
 		return xmp_start_player(self._ctx, freq, mode)
 
+	def play_frame(self):
+		return xmp_play_frame(self._ctx) == 0
+
+	def play_buffer(self, buf, size, loop):
+		return xmp_play_buffer(self._ctx, buf, size, loop)
+
 	def get_frame_info(self, info):
 		return xmp_get_frame_info(self._ctx, pointer(info))
+
+	def end_player(self):
+		xmp_end_player(self._ctx)
+
+	def inject_event(self, chn, event):
+		xmp_inject_event(self._ctx, chn, event)
 
 	def get_module_info(self, info):
 		return xmp_get_module_info(self._ctx, pointer(info))
 
+	def get_format_list(self):
+		# FIXME
+		pass
+
+	def next_position(self):
+		return xmp_next_position(self._ctx)
+
+	def prev_position(self):
+		return xmp_prev_position(self._ctx)
+
+	def set_position(self, num):
+		return xmp_set_position(self._ctx, num)
+
+	def stop_module(self):
+		xmp_stop_module(self._ctx)
+
+	def restart_module(self):
+		xmp_restart_module(self._ctx)
+
+	def seek_time(self, time):
+		return xmp_seek_time(self._ctx, time)
+
+	def channel_mute(self, chn, val):
+		return xmp_channel_mute(self._ctx, chn, val)
+
+	def channel_vol(self, chn, val):
+		return xmp_channel_vol(self._ctx, chn, val)
+
+	def set_player(self, param, value):
+		return xmp_set_player(self._ctx, param, value)
+
+	def get_player(self, param):
+		return xmp_get_player(self._ctx, param)
+
+	def set_instrument_path(self, path):
+		return xmp_set_instrument_path(self._ctx, path)
+	
+	# Extra convenience calls
+
 	def get_buffer(self, info):
 		buf = ctypes.cast(info.buffer, POINTER(c_int8))
 		return ctypes.string_at(buf, info.buffer_size);
-
-	def play_frame(self):
-		return xmp_play_frame(self._ctx) == 0
-
-	def end_player(self):
-		xmp_end_player(self._ctx)
 
 	def get_sample(self, mod, num):
 		sample = mod.xxs[num]
